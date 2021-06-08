964ede037b11c1be23f2855789bd5dea3fb4254f
zhengzhengfr <85538315+zhengzhengfr@users.noreply.github.com>
2021-06-08 23:02:56 +0800

    Update 0constants-cs.h

--- 0constants-cs.h
+++ 0constants-cs.h
@@ -11,45 +11,45 @@
 
 // Water, oil and air properties
 #define VELOCITY                        2.4 //impact velocity in m/s
-#define DROP_RADIUS             		1.295e-3 //m 
-#define DROP_DIAMETER                   (2.*DROP_RADIUS)
-#define DROP_RADIUS_INNER             	0.8695e-3 //m
-#define DROP_DIAMETER_I                 (2.*DROP_RADIUS_INNER)
-#define GAP								0 //m, negative or positive
+#define DROP_RADIUS             	1.295e-3 //m, outer drop radius
+#define DROP_DIAMETER                   (2.*DROP_RADIUS)//m, outer drop diameter
+#define DROP_RADIUS_INNER             	0.8695e-3 //m, inner drop radius
+#define DROP_DIAMETER_I                 (2.*DROP_RADIUS_INNER)//m, inner drop diameter
+#define GAP				0 //m, negative or positive
 
 // Fluid porperties
-#define RHO_W							998.0 // water in kg/m3
-#define RHO_O							913.0 // oil in kg/m3
+#define RHO_W				998.0 // water in kg/m3
+#define RHO_O				913.0 // oil in kg/m3
 #define RHO_A                           1.21  // air in kg/m3
 
-#define MU_W							0.89e-3 	//Pa.s
-#define MU_O							4.57e-3    	//Pa.s
+#define MU_W				0.89e-3 	//Pa.s
+#define MU_O				4.57e-3    	//Pa.s
 #define MU_A                            0.01837e-3 	//Pa.s
 
 #define SIGMA_OA                        2.0e-2 //oil-air  N/m
 #define SIGMA_WO                        4.2e-2 //water-oil N/m
-#define RATIO_SIGMA						SIGMA_OA/SIGMA_WO //=0.4762            
+#define RATIO_SIGMA			SIGMA_OA/SIGMA_WO //=0.4762            
 
 // non-dimensional numbers
-#define REYNOLDS						(RHO_W*VELOCITY*DROP_DIAMETER/MU_W) //based on the fluid properties of water and diameter of oil
-#define WEBER							(RHO_W*VELOCITY*VELOCITY*DROP_DIAMETER/SIGMA_WO) //based on rho water, diameter of oil and sigma w-o
+#define REYNOLDS			(RHO_W*VELOCITY*DROP_DIAMETER/MU_W) //based on the fluid properties of water and diameter of oil
+#define WEBER				(RHO_W*VELOCITY*VELOCITY*DROP_DIAMETER/SIGMA_WO) //based on rho water, diameter of oil and sigma w-o
 
 // simulation set-up
-#define INITAL_GRID_LEVEL		7  	//7	
+#define INITAL_GRID_LEVEL		7   //7	
 #define MAX_GRID_LEVEL		 	12  //maximum grid level
 #define MAX_GEO_LEVEL		 	14  //maximum grid level just before and after the jetting 
-#define JET_TIME				6.0 //dimensionless time when jet occurs (estimated and it's quite accurate)
-#define MAX_TIME				10.0 //simulation time (non-dimensional)
+#define JET_TIME			6.0 //dimensionless time when jet occurs (estimated and it's quite accurate)
+#define MAX_TIME			10.0 //simulation time (non-dimensional)
 #define SAVE_FILE_EVERY			0.005 //dump file output every 0.005
 
 // geometry 
 #define DOMAIN_WIDTH			10e-3 	 //m, 3.8 times the diameter
 #define INITIAL_DISTANCE		0.4e-3   //m, 0.154 times the diameter
-#define REFINE_GAP				0.048e-3 //m, 0.0185 times the diameter
+#define REFINE_GAP			0.048e-3 //m, 0.0185 times the diameter
 #define SHOWITERATION			1
 
 // refine fields VARIABLES
-#define REFINE_VAR				{f1, u.x, u.y, f2} // f1, f2 two tracers and velocity fields
+#define REFINE_VAR			{f1, u.x, u.y, f2} // f1, f2 two tracers and velocity fields
 #define REFINE_VAR_TEXT		  	"f1, u.x, u.y, f2" // not necessary 
 #define REFINE_VALUE_0			-6
 #define REFINE_VALUE_1			-3
@@ -61,11 +61,11 @@
 //removal of small bubbles/droplets
 #define REMOVE_BUBBLE_YESNO		'y'
 #define REMOVE_BUBBLE_SIZE		320.0 // equivalent diameter based on the maximum refinement, 5 is ok for grid (7,9) for the big bubble in the bottom
-#define REMOVE_BUBBLE_PERIOD	10
+#define REMOVE_BUBBLE_PERIOD		10
 
 // Other constant values
-#define R_VOFLIMIT				1.0e-6 
-#define R_PI					3.1415926535897932384626433832795
+#define R_VOFLIMIT			1.0e-6 
+#define R_PI				3.1415926535897932384626433832795
 
 int LEVELmin = INITAL_GRID_LEVEL, LEVELmax = MAX_GRID_LEVEL, LEVELgeo = MAX_GEO_LEVEL;
 double Reynolds = REYNOLDS, Weber = WEBER;
